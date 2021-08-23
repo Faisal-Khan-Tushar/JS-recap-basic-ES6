0804e9aada9caa1135ee4fc7baa043d690f02aa0
Faisal-Khan-Tushar <faisal15-13400@diu.edu.bd>
2021-08-23 13:20:22 +0600

    fifth push

---
+++ template-string.js
@@ -0,0 +1,23 @@
+const sentance = 'This is a sentance'
+const anotherSentance = "this is another way of writing sentance"
+const newSentance = `This is a new way of writing sentance`;
+// console.log(newSentance);
+const oldWay = 'This is the first line..' +
+  'this is the second line.' +
+  'this this a third line.';
+// console.log(oldWay);
+const newWay = `This is the first line
+this is the second line
+this is the third line
+this is the fourth line`;
+// console.log(newWay);
+const count = 5;
+const friends = ['abul', 'babul', 'sabul', 'kabul', 'ramdan'];
+const newFriendOldWay = '<h3 class="friend-name">Friend-5</h3>';
+// console.log(newFriendOldWay);
+const newFriendNewWay = `<h3 class=friend-name">My total friends-${friends.length}.So if one friend pays 50 taka the total amount we will get from them is-${friends.length * 50}</h3>`
+console.log(newFriendNewWay);
+
+
+
+
