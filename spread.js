0f22e2a6d9c8430591a91620fffea872699072c5
Faisal-Khan-Tushar <faisal15-13400@diu.edu.bd>
2021-08-23 15:37:03 +0600

    8th concept

---
+++ spread.js
@@ -0,0 +1,19 @@
+//finding the maximum value within an array
+const numbers = [1, 2, 3, 4, 5];
+// console.log(...numbers);
+const maxNumber = Math.max(1, 2, 3);
+const maxInArray = Math.max(...numbers);
+// console.log(maxInArray);
+
+// numbers.push(100);
+// console.log(numbers);
+// console.log(numbers);
+const numbers2 = [213487234, ...numbers, 20000];
+numbers.push(200);
+
+numbers.pop();
+numbers.pop();
+numbers.push(343343);
+console.log(numbers2);
+console.log(numbers);
+// console.log(numbers);
